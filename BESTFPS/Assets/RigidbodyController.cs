f4de2dab36b6f795763fe0d14349c5e157b73ab2
JinSjauw <jinzhouliu4@gmail.com>
2021-12-02 07:08:09 +0100

    Character Controller fixes
    Fixed Jittery camera, fixed gravity & jump also added acceleration between moving speeds.

--- BESTFPS/Assets/RigidbodyController.cs
+++ BESTFPS/Assets/RigidbodyController.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 public class RigidbodyController : MonoBehaviour
 {
-    private bool isSprinting = false;
+    [SerializeField] private bool isSprinting = false;
 
     [Header("Functional Options")]
     [SerializeField] private bool canSprint = true;
@@ -15,6 +15,9 @@ public class RigidbodyController : MonoBehaviour
     [SerializeField] private bool willSlideOnSlopes = true;
 
     [Header("Movement Parameters")]
+    [SerializeField] private float targetSpeed;
+    [SerializeField] private float currentSpeed = 0;
+    [SerializeField] private float lastSpeed = 0;
     [SerializeField] private float walkSpeed = 5.0f;
     [SerializeField] private float sprintSpeed = 10.0f;
     [SerializeField] private float crouchSpeed = 2.0f;
@@ -28,41 +31,39 @@ public class RigidbodyController : MonoBehaviour
     [SerializeField] private bool shouldJump = true;
 
     [Header("Look Parameters")]
-    [SerializeField, Range(.1f, 10f)] private float lookSpeedX = 2.0f;
-    [SerializeField, Range(.1f, 10f)] private float lookSpeedY = 2.0f;
+    [SerializeField, Range(.01f, .1f)] private float lookSpeedX = 0.05f;
+    [SerializeField, Range(.01f, .1f)] private float lookSpeedY = 0.05f;
     [SerializeField, Range(1, 180)] private float upperLookLimit = 80.0f;
     [SerializeField, Range(1, 180)] private float lowerLookLimit = 80.0f;
 
     [SerializeField] private Vector3 directionVelocity, wantedVelocity;
+    [SerializeField] private Transform playerCamera;
+    [SerializeField] private Transform orientation;
 
     private Rigidbody rigbod;
-    private Camera playerCamera;
+    
     private Vector2 mousePosition;
-
-    private float currentSpeed = 0;
-    private float rotationX = 0;
+    private float rotationX, rotationY = 0;
 
     // Start is called before the first frame update
     void Awake()
     {
-        playerCamera = GetComponentInChildren<Camera>();
+        //playerCamera = GetComponentInChildren<Camera>();
         rigbod = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     public void Move(InputAction.CallbackContext context) 
     {
-        Vector2 movement = context.ReadValue<Vector2>();
-        directionVelocity = new Vector3(movement.x, directionVelocity.y, movement.y);
-        //Debug.Log("Movement Direction: " + directionVelocity);
+            Vector2 movement = context.ReadValue<Vector2>();
+            directionVelocity = new Vector3(movement.x, directionVelocity.y, movement.y);
     }
 
     public void Jump(InputAction.CallbackContext context) 
     {
-        if (context.performed && isGrounded()) 
-        {
-            Debug.Log("Jumped");
+        if(context.performed)
             shouldJump = true;
-        }
     }
 
     public void Sprinting(InputAction.CallbackContext context) 
@@ -71,6 +72,12 @@ public class RigidbodyController : MonoBehaviour
         {
             isSprinting = true;
         }
+
+        if(context.canceled)
+        {
+            isSprinting = false;
+        }
+
     }
 
     public void Fire(InputAction.CallbackContext context) 
@@ -81,78 +88,69 @@ public class RigidbodyController : MonoBehaviour
     public void MouseLook(InputAction.CallbackContext context) 
     {
         mousePosition = context.ReadValue<Vector2>();
-    }
 
-    void HandleMouse() 
-    {
+        rotationY += mousePosition.x * lookSpeedX;
         rotationX -= mousePosition.y * lookSpeedY;
         rotationX = Mathf.Clamp(rotationX, -upperLookLimit, lowerLookLimit);
-        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-        transform.rotation *= Quaternion.Euler(0, mousePosition.x * lookSpeedX, 0);
+
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0);
+        orientation.transform.rotation *= Quaternion.Euler(0, mousePosition.x * lookSpeedX, 0);
     }
 
     void HandleMove()
     {
-        float moveSpeed = (isSprinting ? sprintSpeed : walkSpeed);
-
-        if (directionVelocity == new Vector3(0, 0, 0))
+        targetSpeed = (isSprinting ? sprintSpeed : walkSpeed); 
+        wantedVelocity = orientation.transform.TransformDirection(directionVelocity);
+        
+        if(directionVelocity == Vector3.zero)
         {
-            currentSpeed = currentSpeed - deceleration * Time.fixedDeltaTime;
+            currentSpeed = Mathf.Lerp(currentSpeed, 0, acceleration * Time.fixedDeltaTime) ;
         }
-        else 
+        else
         {
-            wantedVelocity = directionVelocity;
-            currentSpeed = currentSpeed + acceleration * Time.fixedDeltaTime;
+            currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, acceleration * Time.fixedDeltaTime);
         }
-
-        currentSpeed = Mathf.Clamp(currentSpeed, 0, moveSpeed);
-        rigbod.velocity = transform.TransformDirection(wantedVelocity) * currentSpeed;
+        
+        currentSpeed = Mathf.Clamp(currentSpeed, 0, sprintSpeed);
+        wantedVelocity = wantedVelocity * currentSpeed;
+        rigbod.velocity = new Vector3(wantedVelocity.x, rigbod.velocity.y, wantedVelocity.z);
     }
 
     void HandleJump() 
     {
-        if (shouldJump) 
+        if (shouldJump && isGrounded()) 
         {
             Debug.Log("Jumping");
-            directionVelocity.y = jumpPower;
+            rigbod.velocity += new Vector3(0, jumpPower, 0);        
+        }else
+        {
+            shouldJump = false;
         }
     }
 
     void HandleGravity() 
     {
-        if (!isGrounded())
+        if(!isGrounded())
         {
-            Debug.Log("Gravity Applied");
-            directionVelocity.y -= gravity * Time.fixedDeltaTime;
-        }
-        else 
-        {
-            directionVelocity.y = 0f;
-        }
+            rigbod.velocity -= new Vector3(0, gravity * Time.fixedDeltaTime, 0);
+        }   
     }
 
     bool isGrounded() 
     {
         LayerMask groundLayer = LayerMask.GetMask("Ground");
         Debug.DrawRay(transform.position, Vector3.down, Color.blue);
-        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 1.1f, groundLayer))
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 1.2f, groundLayer))
         {
             return true;
         }
-        shouldJump = false;
         return false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        HandleMouse();
     }
 
     void FixedUpdate() 
     {
-        HandleGravity();
-        HandleJump();
         HandleMove();
+        HandleJump();
+        HandleGravity();   
     }
 }
