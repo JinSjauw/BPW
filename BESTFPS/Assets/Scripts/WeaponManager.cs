7af64cb898c1449cbd82c083c218284acf0a08b0
JinSjauw <jinzhouliu4@gmail.com>
2021-12-20 03:54:47 +0100

    Setup for polishing recoil
    Moved WeaponManager higher up in the hierarchy for better access to recoil script.

--- BESTFPS/Assets/Scripts/WeaponManager.cs
+++ BESTFPS/Assets/Scripts/WeaponManager.cs
@@ -16,10 +16,12 @@ public class WeaponManager : MonoBehaviour
     private bool isWeaponHeld;
     private Weapon heldWeapon;
     private WeaponHandler weaponHandler;
+    private Recoil recoilManager;
 
     private void Start() 
     {
         weaponHandler = GetComponentInChildren<WeaponHandler>();
+        recoilManager = GetComponentInChildren<Recoil>();
     }
 
     public void OnPickUp(InputValue context)
